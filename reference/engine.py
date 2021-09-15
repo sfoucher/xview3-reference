f86559431f3931c6cc42bf2b263b18b205dd4326
sfoucher <samuel.foucher@gmail.com>
2021-09-15 12:32:35 -0400

    modif for wandb

--- reference/engine.py
+++ reference/engine.py
@@ -12,7 +12,12 @@ from coco_utils import get_coco_api_from_dataset
 """
 Source: https://github.com/pytorch/vision/blob/master/references/detection/engine.py
 """
+try:
+    import wandb
 
+    assert hasattr(wandb, '__version__')  # verify package import not local dir
+except (ImportError, AssertionError):
+    wandb = None
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     model.train()
@@ -58,8 +63,9 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
 
         if lr_scheduler is not None:
             lr_scheduler.step()
-        #wandb.log(loss_dict_reduced)
-        #wandb.log({'train/total loss':loss_value})
+        if wandb:
+            wandb.log(loss_dict_reduced)
+            wandb.log({'train/total loss':loss_value})
         
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
